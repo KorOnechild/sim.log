452daa661c35fda6a40c275779257488c03fcbe4
KorOnechild <kyuhong99@gmail.com>
2024-07-23 19:11:23 +0900

    Update: Page.astro 게시글 날짜 내림차순으로 가져오기 구현

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,9 +3,20 @@ import { getCollection } from "astro:content";
 export const defMeta = {
   title: "sim.log",
   description: "sim.log",
-  keyword: "sim.log, 기술블로그, 개발블로그",
+  keyword: "sim.log, 기술블로그, 개발블로그, 공부블로그, 블로그, 개발자, 일상",
 };
 
-export const getPosts = async (post) => (
-  await getCollection(post)
-);
+// 게시글 최신순 비교 함수
+const orderByDesc = (a, b) => {
+  return b.data.date - a.data.date;
+};
+
+// 게시글 가져온 후 최신순 정렬
+export const getPosts = async (post) =>
+  (await getCollection(post)).sort(orderByDesc);
+
+// 날짜 포맷 계산 함수
+export const subDate = (date) =>
+  `${date.getFullYear()}-${
+    (date.getMonth() >= 10 ? (date.getMonth() + 1) : `0` + (date.getMonth() + 1))
+  }-${date.getDate() >= 10 ? date.getDate() : `0` + date.getDate()}`;
