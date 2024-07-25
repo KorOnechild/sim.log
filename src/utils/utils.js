b47b3e644840e44ff43e38b699176ba944477027
KorOnechild <kyuhong99@gmail.com>
2024-07-26 00:32:18 +0900

    Remove: base

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,4 @@
 import { getCollection } from "astro:content";
-import defineConfig from "../../astro.config.mjs";
-
-export const base = defineConfig.base;
 
 export const defMeta = {
   title: "sim.log",
