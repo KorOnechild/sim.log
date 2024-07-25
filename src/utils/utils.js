c7af2a624f25ab6a267f8c005e53ea7c3d3313e0
KorOnechild <kyuhong99@gmail.com>
2024-07-25 16:58:22 +0900

    Chore: SEO 작업 meta 태그, open graph, twitter og 작업

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,7 +5,7 @@ export const base = defineConfig.base;
 
 export const defMeta = {
   title: "sim.log",
-  description: "sim.log",
+  description: "개발 블로그 sim.log",
   keyword: "sim.log, 기술블로그, 개발블로그, 공부블로그, 블로그, 개발자, 일상",
 };
 
