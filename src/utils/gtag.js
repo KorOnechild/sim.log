23289f6d9aa5e5e7f3186357d6fef1db9dffcf56
KorOnechild <kyuhong99@gmail.com>
2024-07-25 11:23:04 +0900

    Update: gtag 설정

---
+++ src/utils/gtag.js
@@ -0,0 +1,12 @@
+// Google tag (gtag.js)
+<script
+  async
+  src="https://www.googletagmanager.com/gtag/js?id=G-PQ2BHTQ7SH"
+></script>;
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "G-PQ2BHTQ7SH");
