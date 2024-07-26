e6b7b19ff8cf08a16e9d6b2c57265b822ed53629
KorOnechild <kyuhong99@gmail.com>
2024-07-26 17:07:19 +0900

    fix: dependecies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from "astro/config";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://simlog.life",
-  integrations: [react()],
 });
