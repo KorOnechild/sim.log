8b0737ec86128253704d32527b3b3a4ba3fabf0c
KorOnechild <kyuhong99@gmail.com>
2024-07-27 17:53:48 +0900

    Update: darkmode color-scheme 적용 및 dark 모드 적용을 html 수준으로 옮김

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,17 +1,18 @@
 document.addEventListener("astro:page-load", () => {
-  const bodyClassName = document.getElementsByTagName("body")[0].classList;
+  const htmlClassName = document.getElementsByTagName("html")[0].classList;
+  // const bodyClassName = document.getElementsByTagName("body")[0].classList;
   const themeButtons = document.getElementById("theme-button");
   const theme = localStorage.getItem("theme");
-  bodyClassName.value = theme;
+  htmlClassName.value = theme;
 
   const changeTheme = () => {
     const theme = localStorage.getItem("theme");
     if (theme === "light") {
       localStorage.setItem("theme", "dark");
-      bodyClassName.value = "dark";
+      htmlClassName.value = "dark";
     } else {
       localStorage.setItem("theme", "light");
-      bodyClassName.value = "light";
+      htmlClassName.value = "light";
     }
     console.log("hi");
   };
