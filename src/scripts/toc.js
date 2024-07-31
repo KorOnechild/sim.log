4ba883bac3205f01e935fefb99cdf405c29f2a6e
KorOnechild <kyuhong99@gmail.com>
2024-07-31 17:25:17 +0900

    Fix: TOC 렌더링 문제 수정

--- src/scripts/toc.js
+++ src/scripts/toc.js
@@ -1,4 +1,76 @@
 document.addEventListener("astro:page-load", () => {
+  const getBody = (headLine) => {
+    let temp_html = "";
+    let isOpenedNestedUl = false;
+    // TOC 생성 함수
+    const getTree = (
+      arr,
+      curIdx,
+      isPrevH1,
+      isCurH1,
+      isNextH1,
+      temp_html,
+      isOpenedNestedUl
+    ) => {
+      // 탈출 조건
+      if (arr.length === curIdx) {
+        temp_html = "<ul>" + temp_html + "</ul>";
+        return temp_html;
+      }
+      const re = /[!?.]/g;
+      const curValue = arr[curIdx];
+      const curText = curValue.replaceAll("#", "").trimStart();
+      const curId = curValue
+        .replaceAll("#", "")
+        .toLowerCase()
+        .trimStart()
+        .replaceAll(re, "")
+        .replaceAll(" ", "-");
+      isPrevH1 =
+        arr[curIdx - 1] !== undefined
+          ? arr[curIdx - 1].charAt(1) === " "
+          : null;
+      isCurH1 = curValue.charAt(1) === " ";
+      isNextH1 =
+        arr[curIdx + 1] !== undefined
+          ? arr[curIdx + 1].charAt(1) === " "
+          : null;
+
+      if (isPrevH1 && isPrevH1 !== null && !isCurH1) {
+        temp_html = temp_html + '<ul class="toc ul">';
+        isOpenedNestedUl = true;
+      }
+
+      temp_html =
+        temp_html +
+        `<li class="toc li"><a href="#${curId}">${curText}</a></li>`;
+
+      if (isOpenedNestedUl && (isNextH1 || isNextH1 === null)) {
+        temp_html = temp_html + "</ul>";
+        isOpenedNestedUl = false;
+      }
+      return getTree(
+        arr,
+        curIdx + 1,
+        isPrevH1,
+        isCurH1,
+        isNextH1,
+        temp_html,
+        isOpenedNestedUl
+      );
+    };
+
+    return getTree(headLine, 0, null, true, null, temp_html, isOpenedNestedUl);
+  };
+
+  const tocContainer = document.getElementsByClassName("toc container")[0];
+
+  if (tocContainer !== undefined) {
+    const body = tocContainer.dataset.body.split(",");
+    const temp_toc = getBody(body);
+    tocContainer.innerHTML = temp_toc;
+  }
+
   const tags = document.querySelectorAll(".markdown > h1, .markdown > h2");
   const lis = document.querySelectorAll(".toc.li");
   const footer = document.querySelector("footer");
@@ -31,12 +103,11 @@ document.addEventListener("astro:page-load", () => {
     rootMargin: "0px 0px -299px 0px",
   };
 
-  if (document.getElementsByClassName("toc container").length !== 0) {
-    const toc = document.getElementsByClassName("toc container");
+  if (tocContainer !== undefined) {
     const footerObserver = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (!entry.isIntersecting) {
-          toc[0].style.visibility = "";
+          tocContainer.style.visibility = "";
           document.addEventListener("astro:page-load", () => {
             const tags = document.querySelectorAll(
               ".markdown > h1, .markdown > h2"
@@ -72,13 +143,13 @@ document.addEventListener("astro:page-load", () => {
             };
 
             if (document.getElementsByClassName("toc container").length !== 0) {
-              const toc = document.getElementsByClassName("toc container");
+              
               const footerObserver = new IntersectionObserver((entries) => {
                 entries.forEach((entry) => {
                   if (!entry.isIntersecting) {
-                    toc[0].style.visibility = "";
+                    tocContainer.style.visibility = "";
                   } else if (entry.isIntersecting) {
-                    toc[0].style.visibility = "hidden";
+                    tocContainer.style.visibility = "hidden";
                   }
                 });
               }, footerOptions);
@@ -86,7 +157,7 @@ document.addEventListener("astro:page-load", () => {
             }
           });
         } else if (entry.isIntersecting) {
-          toc[0].style.visibility = "hidden";
+          tocContainer.style.visibility = "hidden";
         }
       });
     }, footerOptions);
